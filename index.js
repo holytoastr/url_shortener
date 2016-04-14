937a4e89af9459e9427bc671714957299bbc045a
Melissa Miller <lis.d.miller@gmail.com>
2016-04-14 10:12:51 -0400

    Sets up handlebars

--- index.js
+++ index.js
@@ -2,3 +2,24 @@ var express   = require("express");
 var hbs       = require("express-handlebars");
 var mongoose  = require("mongoose");
 var parser    = require("body-parser");
+
+var app       = express();
+
+app.set("port", process.env.PORT || 3001);
+app.set("view engine", "hbs");
+
+app.engine(".hbs", hbs({
+  extname:        ".hbs",
+  partialsDir:    "views/",
+  layoutDir:      "views/",
+  defaultLayout:  "layout-main"
+})); // end app handlebars engine
+
+app.get("/", function(req, res){
+  res.render("TBD");
+
+}); // end index view
+
+app.listen(app.get("port"), function(){
+  console.log("Boo!");
+}); // end listen
