e2ea970db6d383f753e31363b15825b31728eed6
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-04-14 15:43:45 -0400

    working app

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,13 @@
-document.getElementById("submitbutton").addEventListener("click", function(){
+var short_url;
+
+document.getElementById("submitbutton").addEventListener("click", makeShort())
+
+function makeShort(){
   var short_url = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   for(var i=0; i < 4; i++){
     short_url += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   document.getElementById("hiddenFieldName").value = short_url;
-});
+  document.getElementById("temp_short").innerHTML = short_url;
+}
